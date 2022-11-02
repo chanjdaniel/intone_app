ad4507b8ad793d12dc9a1680fd21c30443b1546a
Daniel Chan <chanjdaniel@gmail.com>
2022-11-01 19:37:50 -0700

    Initial Commit

---
+++ Intone/IntoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntoneApp.swift
+//  Intone
+//
+//  Created by Daniel Chan on 2022-11-01.
+//
+
+import SwiftUI
+
+@main
+struct IntoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
